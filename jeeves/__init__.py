f889d107b6110dea65d936842e12763736e63b10
Gerard Toonstra <gtoonstra@users.noreply.github.com>
2023-11-24 18:22:13 +0100

    [CX-2086]: Add jeeves to helm charts (#11)
    Co-authored-by: Chiel Fernhout <fernhoutchiel@gmail.com>

---
+++ jeeves/__init__.py
@@ -0,0 +1,20 @@
+# noqa
+"""
+Jeeves
+
+Before using this package, install the dependencies:
+
+    pip install -r jeeves/requirements.txt
+
+Optional:
+
+    pip install typer-cli
+    typer --install-completion
+
+To get help, use:
+
+    j
+"""
+
+# Sub-commands
+from jeeves.dev import dev
---
+++ jeeves/dev.py
@@ -0,0 +1,81 @@
+import enum
+import os
+import sh
+from typer import Typer
+
+
+class EnvVar(enum.StrEnum):
+    DATAFOLD_K8S_SECRETFILE = "DATAFOLD_K8S_SECRETFILE"
+    DATAFOLD_DEPLOY_NAME = "DATAFOLD_DEPLOY_NAME"
+    DATAFOLD_DEV_OVERRIDES = "DATAFOLD_DEV_OVERRIDES"
+    DATAFOLD_DB_HOSTPATH = "DATAFOLD_DB_HOSTPATH"
+    TAG = "TAG"
+
+
+dev = Typer(
+    no_args_is_help=True, help='Commands useful for local development environment.'
+)
+
+
+def _check_env_var(name: str):
+    val = os.getenv(name)
+    if not val:
+        raise Exception(f"Env var {name} is not present, but is required.")
+
+
+def _check_env_vars_present():
+    for name in EnvVar:
+        _check_env_var(name)
+
+
+def _common_args():
+    return [
+        os.getenv(EnvVar.DATAFOLD_DEPLOY_NAME),
+        './charts/datafold',
+        '-f',
+        os.getenv(EnvVar.DATAFOLD_K8S_SECRETFILE),
+        '-f',
+        os.getenv(EnvVar.DATAFOLD_DEV_OVERRIDES),
+        '--set',
+        f'global.hostPath={os.getenv(EnvVar.DATAFOLD_DB_HOSTPATH)}',
+        '--set',
+        f'global.datafoldVersion={os.getenv(EnvVar.TAG)}',
+    ]
+
+
+@dev.command()
+def install():
+    """Installs all helm charts."""
+    _check_env_vars_present()
+
+    args = ['--install']
+    args.extend(_common_args())
+
+    out = sh.helm.upgrade(args)
+    print(out)
+
+
+@dev.command()
+def update():
+    """Updates the deployment."""
+    _check_env_vars_present()
+    out = sh.helm.upgrade(_common_args())
+    print(out)
+
+
+@dev.command()
+def uninstall():
+    """Uninstalls the deployment."""
+    _check_env_vars_present()
+    out = sh.helm.uninstall(os.getenv(EnvVar.DATAFOLD_DEPLOY_NAME))
+    print(out)
+
+
+@dev.command()
+def render():
+    """Renders all charts to console output for validation."""
+    _check_env_vars_present()
+    args = ['--debug']
+    args.extend(_common_args())
+    out = sh.helm.template(args)
+    print(out)
